400fed674da3154571f5b48f703750f6eb606e40
Tim Sherry <tsherry82@gmail.com>
2019-09-11 23:44:00 -0400

    uploading to heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,8 +11,16 @@ module.exports = function (app) {
 
         newFriendData = req.body;
         
+        var scoresArr = parseInt(newFriendData.scores);
 
         friendsArr.push(newFriendData);
+
+        for (var i = 0; i < newFriendData.length; i++) {
+            scoresArr = newFriendData[i].scores;
+            for (var i = 0; i < scoresArr.length; i++) {
+                console.log(parseInt(scoresArr[i]));
+            }
+        }
 
         res.json(newFriendData);
 
